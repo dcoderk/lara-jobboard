7d854578eadf6859cdd62900d52da34d3a1c6483
dcoderk <webmarket2k@gmail.com>
2024-07-29 15:40:50 +0800

    adding factory, relationship and pivot table

--- database/migrations/2024_07_28_144610_create_job_listings_table.php
+++ database/migrations/2024_07_28_144610_create_job_listings_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listing', function (Blueprint $table) {
+        Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            //$table->unsignedBigInteger('employer_id);
+            $table->foreignIdFor(\App\Models\Employer::class);
+            $table->string('title');
+            $table->string('salary');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listing');
+        Schema::dropIfExists('job_listings');
     }
 };
